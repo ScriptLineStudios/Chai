a301f03a6e801f577f56042d44b767085639aeb0
ScriptLineStudios <scriptlinestudios@protonmail.com>
2022-10-16 11:03:49 +0200

    fully switched from void *'s to NodeReturns

--- src/parser.c
+++ src/parser.c
@@ -16,32 +16,40 @@ typedef enum {
 String current_token;
 
 typedef struct {
-    void *left;
+    void *node;
+    NodeType node_type;
+} NodeReturn;
+
+typedef struct {
+    NodeReturn left;
     String op;
-    void *right;
+    NodeReturn right;
 } BinOp;
 
 typedef struct {
     int value;
 } Number;
 
-typedef struct {
-    void *node;
-    NodeType node_type;
-} NodeReturn;
-
 void advance_symbol(String *tokens) {
     token_index += 1;
     current_token = tokens[token_index];
 }
 
-void *create_bin_op_node(void *left, String op, void *right) {
+NodeReturn return_node(void *node, NodeType node_type) {
+    NodeReturn ret;
+    ret.node = node;
+    ret.node_type = node_type;
+
+    return ret;
+}
+
+NodeReturn create_bin_op_node(NodeReturn left, String op, NodeReturn right) {
     BinOp *result = (BinOp*)malloc(sizeof(BinOp));
 
     result->left = left;
     result->op = op;
     result->right = right;
-    return result;
+    return return_node((void *)result, BINOP);
 }
 
 int isnumber(char *input) {
@@ -51,14 +59,6 @@ int isnumber(char *input) {
         return 0;
     }
     return 1;
-}
-
-NodeReturn return_node(void *node, NodeType node_type) {
-    NodeReturn ret;
-    ret.node = node;
-    ret.node_type = node_type;
-
-    return ret;
 }
 
 NodeReturn factor(String *tokens) {
@@ -74,45 +74,71 @@ NodeReturn factor(String *tokens) {
 }
 
 NodeReturn term(String *tokens) {
-    NodeReturn _left = factor(tokens);
-    void *left = _left.node;
+    NodeReturn left = factor(tokens);
 
     while (current_token.str != NULL && (strcmp(current_token.str, "*") == 0 || strcmp(current_token.str, "/") == 0)) {
         String op = current_token;
         advance_symbol(tokens);
 
-        NodeReturn _right = factor(tokens);
-        void *right = _right.node;
-        
+        NodeReturn right = factor(tokens);
+
         left = create_bin_op_node(left, op, right);
     }
-    return return_node(left, BINOP);
+    return left;
 }
 
 NodeReturn expression(String *tokens) {
-    NodeReturn _left = factor(tokens);
-    void *left = _left.node;
+    NodeReturn left = term(tokens);
 
     while (current_token.str != NULL && (strcmp(current_token.str, "+") == 0 || strcmp(current_token.str, "-") == 0)) {
         String op = current_token;
         advance_symbol(tokens);
 
-        NodeReturn _right = factor(tokens);
-        void *right = _right.node;
+        NodeReturn right = term(tokens);
 
         left = create_bin_op_node(left, op, right);
     }
-    return return_node(left, BINOP);
+    return left;
 }
 
 void generate_ast(String *tokens) {
     advance_symbol(tokens);
     NodeReturn node = expression(tokens);
-    BinOp *bin_op = node.node;
+    printf("%d\n", node.node_type); //We now know this is a bin op
+    BinOp *bin_op = (BinOp *)node.node;    
 
-    Number *left = (Number *)bin_op->left;
-    Number *right = (Number *)bin_op->right;
+    NodeReturn left = bin_op->left;
+    printf("%d\n", left.node_type); //We now know this is a number node
+    Number *left_number = (Number *)left.node;
 
-    printf("%d\n", left->value);
-    printf("%d\n", right->value);   
+    NodeReturn right = bin_op->right;
+    printf("%d\n", right.node_type); //We now know this is bin op
+    BinOp *right_binop = (BinOp *)right.node;
+
+    NodeReturn right_left = right_binop->left;
+    printf("%d\n", right_left.node_type); //We now know this is a number node
+    Number *right_left_number = (Number *)right_left.node;
+
+    NodeReturn right_right = right_binop->right;
+    printf("%d\n", right_right.node_type); //We now know this is a number node
+    Number *right_right_number = (Number *)right_right.node;
+
+    printf("%d %d %d \n", left_number->value, right_left_number->value, right_right_number->value);
+
+
+    // NodeReturn node = expression(tokens);
+    // printf("%d\n", node.node_type); //We now know this is a bin op
+    // BinOp *bin_op = (BinOp *)node.node;
+    
+    // NodeReturn left = bin_op->left;
+    // printf("%d\n", left.node_type); //We now know this is a number node
+    // Number *left_number = (Number *)left.node;
+
+    // NodeReturn right = bin_op->right;
+    // printf("%d\n", right.node_type); //We now know this is also a number node
+    // Number *right_number = (Number *)right.node;
+    
+
+    // printf("LEFT = %d\n", left_number->value);
+    // printf("RIGHT = %d\n", right_number->value);
 }
